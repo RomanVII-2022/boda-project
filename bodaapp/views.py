c55709dd9c5ce5af23e157dc9ece1e42511d0ed8
RomanVII-2022 <vicmkinyua77@gmail.com>
2023-10-25 06:32:21 +0300

    Shipper edit view

--- bodaapp/views.py
+++ bodaapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from bodaapp.serializers import PersonnelSerializer, ShipperSerializer, CarrierSerializer, VehicleTypeSerializer, VehicleSerializer, OrderSerializer
 from bodaapp.models import Personnel, Shipper, Carrier, VehicleType, Vehicle, Order
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response 
 
 class PersonnelViewSet(viewsets.ModelViewSet):
     serializer_class = PersonnelSerializer
@@ -11,6 +12,19 @@ class PersonnelViewSet(viewsets.ModelViewSet):
 class ShipperViewSet(viewsets.ModelViewSet):
     serializer_class = ShipperSerializer
     queryset = Shipper.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = ShipperSerializer(data=request.data)
+        if serializer.is_valid():
+            # shipper = Shipper(full_name = serializer.validated_data['full_name'], 
+            #                   email = serializer.validated_data['email'], 
+            #                   phone_number = serializer.validated_data['phone_number'], 
+            #                   business_or_person = serializer.validated_data['business_or_person']
+            #                   )
+            # shipper.set_password(serializer.validated_data['password'])
+            # shipper.save()
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CarrierViewSet(viewsets.ModelViewSet):
